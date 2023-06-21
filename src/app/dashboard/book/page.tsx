c1099db16a1f81d2ab5acc33a5e037108dcb6ddb
aagii1104 <b200930072@erdenetis.edu.mn>
2023-06-21 14:21:10 +0800

    added book search and delete

--- src/app/dashboard/book/page.tsx
+++ src/app/dashboard/book/page.tsx
@@ -1,29 +1,96 @@
 "use client";
 import Book from "@library/components/common/Book";
-import { Grid } from "@mui/material";
+import { Grid, MenuItem, Pagination } from "@mui/material";
 import axios from "axios";
 import React, { FC, useEffect, useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { AddOutlined, SearchOutlined } from "@mui/icons-material";
+import { useFormik } from "formik";
+import { useRouter } from "next/navigation";
+import { IBook } from "@library/types";
 
 const BookPage: FC<any> = () => {
-    const [books, setBooks] = useState([]);
+    const [books, setBooks] = useState<Array<IBook>>([]);
+    const router = useRouter();
+    const [page, setPage] = useState<number>(0);
+    const [size, setSize] = useState<number>(100);
+    const [totalPage, setTotalPage] = useState<number>(1);
 
-    const fetchBooks = async () => {
+    const fetchBooks = async (values: any) => {
         try {
-            const response = await axios.post("/api/book/find", {});
+            const response = await axios.post("/api/book/find", values, { params: { page, size } });
             setBooks(response.data.content);
+            setTotalPage(response.data.totalPage);
         } catch (error) {}
+        [page, size];
     };
 
     useEffect(() => {
-        fetchBooks();
-    }, []);
+        fetchBooks({});
+        //eslint-disable-next-line
+    }, [page, size]);
+
+    const form = useFormik({
+        initialValues: {},
+        onSubmit: fetchBooks,
+    });
 
     return (
         <Grid container spacing={2}>
+            <Grid item xs={12} marginTop={"20px"}>
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Typography>Номын жагсаалт</Typography>
+                    <Button
+                        variant="contained"
+                        onClick={() => router.push("/dashboard/book/register")}
+                        startIcon={<AddOutlined />}
+                    >
+                        Ном бүртгэх
+                    </Button>
+                </Box>
+            </Grid>
+            <Grid item xs={12}>
+                <form onSubmit={form.handleSubmit}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6} lg={3}>
+                            <TextField fullWidth label="Нэр" size="small" name="name" onChange={form.handleChange} />
+                        </Grid>
+                        <Grid item xs={6} lg={3}>
+                            <TextField
+                                fullWidth
+                                label="Зохиолч"
+                                size="small"
+                                name="author"
+                                onChange={form.handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={6} lg={3} container spacing={3}>
+                            <Button size="small" startIcon={<SearchOutlined />} type="submit">
+                                Хайх
+                            </Button>
+                        </Grid>
+                        <Box display="flex" justifyContent="space-between" alignItems="center" marginLeft={"40px"}>
+                            <TextField size="small" select value={size} onChange={(e: any) => setSize(e.target.value)}>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={8}>8</MenuItem>
+                                <MenuItem value={16}>16</MenuItem>
+                                <MenuItem value={40}>40</MenuItem>
+                            </TextField>
+                            <Pagination
+                                count={totalPage}
+                                page={page + 1}
+                                onChange={(e: any, val: number) => setPage(val - 1)}
+                                color="primary"
+                            />
+                        </Box>
+                    </Grid>
+                </form>
+            </Grid>
             {books.map((book: any, index: number) => {
                 return (
                     <Grid item xs={6} lg={3} key={index}>
-                        <Book {...book} />
+                        <Book {...book} fetchBooks={fetchBooks} />
                     </Grid>
                 );
             })}
--- src/components/common/Book.tsx
+++ src/components/common/Book.tsx
@@ -1,25 +1,126 @@
-import { Box, Card, Typography } from "@mui/material";
-import Image from "next/image";
-import React, { FC } from "react";
+import {
+    Box,
+    Card,
+    Typography,
+    Button,
+    IconButton,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+} from "@mui/material";
+import React, { FC, useCallback, useState } from "react";
+import CardContent from "@mui/material/CardContent";
+import { useRouter } from "next/navigation";
+import { Delete, Edit } from "@mui/icons-material";
+import axios from "axios";
 
 interface BookProps {
     isbn: string;
     name: string;
+    description: string;
     coverUrl: string;
+    _id: string;
+    fetchBooks: any;
 }
 
-const Book: FC<BookProps> = ({ isbn, name, coverUrl }) => {
+const Book: FC<BookProps> = ({ isbn, name, coverUrl, description, _id, fetchBooks }) => {
+    const [showMore, setShowMore] = useState(false);
+    const router = useRouter();
+    const [page, setPage] = useState<number>(0);
+    const [size, setSize] = useState<number>(100);
+    const [totalPage, setTotalPage] = useState<number>(1);
+    const [selectedBook, setSelectedBook] = useState<string | null>(null); //nomiin id avna
+    const handleClick = () => {
+        setShowMore(!showMore);
+    };
+    const handleClickOpen = (bookId: string) => {
+        setSelectedBook(bookId);
+        setOpen(true);
+    };
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const deleteUser = useCallback(
+        async (bookId: string) => {
+            console.log(bookId);
+            try {
+                await axios.delete(`/api/book/${selectedBook}`); //temdeglegdsen idg selected duudaj avna
+                fetchBooks({});
+                handleClose();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        [fetchBooks, selectedBook]
+    );
+
     return (
-        <Card elevation={4} sx={{ padding: 0, borderRadius: 2 }}>
+        <Card elevation={5} sx={{ padding: 0, borderRadius: 5 }} style={{ height: "100%" }}>
+            <CardContent style={{ backgroundColor: "black" }}>
+                <Typography align="center" color="common.white">
+                    {isbn}
+                </Typography>
+            </CardContent>
             {
                 // eslint-disable-next-line
-            }<img alt={name} src={"/public/uploads/" + coverUrl} style={{ width: "100%" }} />
-            <Box padding={2}>
-                <Typography>{isbn}</Typography>
-                <Typography>{name}</Typography>
-            </Box>
+            }
+            <CardContent style={{ width: "120px", height: "240px", justifyContent: "center", alignItems: "center" }}>
+                <img alt={name} src={"/public/uploads/" + coverUrl} style={{ width: "150px" }} />
+            </CardContent>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {name}
+                </Typography>
+                {showMore ? (
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                ) : (
+                    <Typography variant="body2" color="text.secondary">
+                        {description.slice(0, 100)}...
+                    </Typography>
+                )}
+                <Button variant="outlined" onClick={() => router.push("/dashboard/book/detail")}>
+                    Show More
+                </Button>
+                <IconButton>
+                    <Edit onClick={() => router.push("/dashboard/book/update")} />
+                </IconButton>
+                <IconButton color="error" onClick={() => handleClickOpen(_id)}>
+                    <Delete />
+                </IconButton>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Ном устгах"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Та дараах ном устгахыг зөвшөөрч байна уу?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Үгүй</Button>
+                        <Button onClick={() => deleteUser(_id)}>Тийм</Button>
+                    </DialogActions>
+                </Dialog>
+            </CardContent>
         </Card>
     );
 };
 
 export default Book;
+function setBooks(content: any) {
+    throw new Error("Function not implemented.");
+}
+
+function setOpen(arg0: boolean) {
+    throw new Error("Function not implemented.");
+}
